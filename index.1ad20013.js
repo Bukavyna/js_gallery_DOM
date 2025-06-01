ce5efe9dff1235bc759ec0082945867f13d0d2b7
Taras <luchakt2@gmail.com>
2025-06-01 13:04:23 +0300

    make build

---
+++ index.1ad20013.js
@@ -0,0 +1,2 @@
+document.getElementById("thumbs").addEventListener("click",function(e){e.preventDefault();var t=e.target.closest("a");if(t){var n=t.href;document.getElementById("largeImg").src=n}});
+//# sourceMappingURL=index.1ad20013.js.map
